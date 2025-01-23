66c5c3922c69c45cfc07ec3ad7967cc70c55bcd4
Alext_v <sharman2fire.90@yandex.ru>
2025-01-23 21:25:44 +0300

    refactor: перенесена логика по созданию и размещению на поле существ из Map.java в Actions.java. Реализован метод initActions()

--- src/com/example/simulation/Actions.java
+++ src/com/example/simulation/Actions.java
@@ -1,6 +1,73 @@
 package com.example.simulation;
 
+import com.example.simulation.Entitys.*;
+
+import java.util.Random;
+
 public class Actions { //действия
+    private final Map map;
     //initActions - действия, совершаемые перед стартом симуляции. Пример - расставить объекты и существ на карте
     //turnActions - действия, совершаемые каждый ход. Примеры - передвижение существ, добавить травы или травоядных, если их осталось слишком мало
+
+    public Actions(Map map) {
+        this.map = map;
+    }
+
+    public void initActions() {
+        setupEntitiesPositions();
+    }
+
+    private void setupEntitiesPositions() { //настройка позиций сущностей
+        createEntity(new Rock(generateRandomCoordinates(map.getRows(), map.getColumns())), 7);
+        createEntity(new Tree(generateRandomCoordinates(map.getRows(), map.getColumns())), 12);
+        createEntity(new Grass(generateRandomCoordinates(map.getRows(), map.getColumns())), 17);
+        createEntity(new Herbivore(generateRandomCoordinates(map.getRows(), map.getColumns())), 7);
+        createEntity(new Predator(generateRandomCoordinates(map.getRows(), map.getColumns())), 3);
+    }
+
+    private void createEntity(Entity entity, int count){
+        Coordinates coordinates;
+
+        for (int i = 0; i < count; i++) {
+            if (entity instanceof Rock){
+                coordinates = generateRandomCoordinates(map.getRows(), map.getColumns());
+                Rock rock = new Rock(coordinates);
+                map.getEntities().put(coordinates, rock);
+            } else if (entity instanceof Tree) {
+                coordinates = generateRandomCoordinates(map.getRows(), map.getColumns());
+                Tree tree = new Tree(coordinates);
+                map.getEntities().put(coordinates, tree);
+            } else if (entity instanceof Grass) {
+                coordinates = generateRandomCoordinates(map.getRows(), map.getColumns());
+                Grass grass = new Grass(coordinates);
+                map.getEntities().put(coordinates, grass);
+            } else if (entity instanceof Herbivore) {
+                coordinates = generateRandomCoordinates(map.getRows(), map.getColumns());
+                Herbivore herbivore = new Herbivore(coordinates);
+                map.getEntities().put(coordinates, herbivore);
+            } else if (entity instanceof Predator) {
+                coordinates = generateRandomCoordinates(map.getRows(), map.getColumns());
+                Predator predator = new Predator(coordinates);
+                map.getEntities().put(coordinates, predator);
+            }
+        }
+    }
+
+    private Coordinates generateRandomCoordinates(int rows, int columns){
+        Random random = new Random();
+        Coordinates coordinates;
+
+        while (true) {
+            int x = random.nextInt(rows + 1);
+            int y = random.nextInt(columns + 1);
+
+            if ((x == 0) || (y == 0)) continue;
+
+            coordinates = new Coordinates(x, y);
+
+            if (!(map.getEntities().containsKey(coordinates))) {
+                return coordinates;
+            }
+        }
+    }
 }
--- src/com/example/simulation/Main.java
+++ src/com/example/simulation/Main.java
@@ -3,7 +3,8 @@ package com.example.simulation;
 public class Main {
     public static void main(String[] args) {
         Map map = new Map(10,20);
-        map.setupEntitiesPositions();
+        Actions actions = new Actions(map);
+        actions.initActions();
         MapConsoleRenderer mapConsoleRenderer = new MapConsoleRenderer();
         mapConsoleRenderer.render(map);
         int a = 123;
