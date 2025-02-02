bbcc4f475b20901d58810e69bedb6aea3a633a75
Alext_v <sharman2fire.90@yandex.ru>
2025-02-02 17:49:27 +0300

    test

--- src/com/example/simulation/Main.java
+++ src/com/example/simulation/Main.java
@@ -1,45 +1,73 @@
 package com.example.simulation;
 
+import com.example.simulation.Entitys.Grass;
 import com.example.simulation.Entitys.Herbivore;
 import com.example.simulation.Entitys.Predator;
 import com.example.simulation.Entitys.Tree;
 
-import java.util.Arrays;
-import java.util.LinkedHashSet;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Simulation simulation = new Simulation(5, 5); //10 20
+        Simulation simulation = new Simulation(10, 20); //10 20
         simulation.gameLoop();
         int a = 123;
 
-//        Map map = new Map(10, 20);
+//        Map map = new Map(10, 10);
 //
-//        Coordinates coordinates = new Coordinates(1, 1);
+//        Coordinates coordinates = new Coordinates(10, 10);
 //        Predator predator = new Predator(coordinates);
 //        map.getEntities().put(coordinates, predator);
 //        map.setCreatures(predator);
-//
-//        Coordinates coordinates1 = new Coordinates(6, 9);
+////
+//        Coordinates coordinates1 = new Coordinates(1, 1);
 //        Herbivore herbivore = new Herbivore(coordinates1);
 //        map.getEntities().put(coordinates1, herbivore);
 //        map.setCreatures(herbivore);
 //
-//        Coordinates coordinates2 = new Coordinates(2, 1);
-//        Tree tree = new Tree(coordinates2);
-//        map.getEntities().put(coordinates2, tree);
-//
-//        Coordinates coordinates3 = new Coordinates(6, 2);
-//        Tree tree1 = new Tree(coordinates3);
-//        map.getEntities().put(coordinates3, tree1);
-//
+//        Coordinates coordinates2 = new Coordinates(4, 9);
+//        Grass grass = new Grass(coordinates2);
+//        map.getEntities().put(coordinates2, grass);
+////
+////        Coordinates coordinates3 = new Coordinates(6, 2);
+////        Tree tree1 = new Tree(coordinates3);
+////        map.getEntities().put(coordinates3, tree1);
+////
 //        MapConsoleRenderer mapConsoleRenderer = new MapConsoleRenderer();
 //        mapConsoleRenderer.render(map);
-////
+//
 //        LinkedHashSet<Coordinates> path = PathSearchAlgorithm.getPath(coordinates, predator.getFood(), map);
-//        Coordinates[] xxx = path.toArray(new Coordinates[0]);
-//        List<Coordinates> fff = Arrays.stream(xxx).toList().reversed();
+//        List<Coordinates> pathList = new ArrayList<>(path);
+//        Collections.reverse(pathList);
+//        System.out.println("Хищник" + pathList);
+//
+//        LinkedHashSet<Coordinates> pathHerbivore = PathSearchAlgorithm.getPath(coordinates1, herbivore.getFood(), map);
+//        List<Coordinates> pathListHerbivore = new ArrayList<>(pathHerbivore);
+//        Collections.reverse(pathListHerbivore);
+//        System.out.println("Кролик" + pathListHerbivore);
+//
+//        int count = 1;
+//        Actions actions = new Actions(map);
+//
+//        while (count < 10) {
+//            actions.turnActions();
+//            mapConsoleRenderer.render(map);
+//
+//            path = PathSearchAlgorithm.getPath(coordinates, predator.getFood(), map);
+//            pathList = new ArrayList<>(path);
+//            Collections.reverse(pathList);
+//            System.out.println("Хищник" + pathList);
+//
+//            pathHerbivore = PathSearchAlgorithm.getPath(coordinates1, herbivore.getFood(), map);
+//            pathListHerbivore = new ArrayList<>(pathHerbivore);
+//            Collections.reverse(pathListHerbivore);
+//            System.out.println("Кролик" + pathListHerbivore);
+//
+//            count++;
+//        }
+//
+////        Coordinates[] xxx = path.toArray(new Coordinates[0]);
+////        List<Coordinates> fff = Arrays.stream(xxx).toList().reversed();
 //        int a = 123;
     }
 }
