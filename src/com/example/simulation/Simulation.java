9223a618cdb04cff794eff4d6e23ec54ed4e30bb
Alext_v <sharman2fire.90@yandex.ru>
2025-02-18 20:35:24 +0300

    feat: time point

--- src/com/example/simulation/Simulation.java
+++ src/com/example/simulation/Simulation.java
@@ -6,25 +6,13 @@ import com.example.simulation.actions.spawn_action.RespawnHerbivoreAction;
 import com.example.simulation.actions.spawn_action.RespawnPredatorAction;
 import com.example.simulation.actions.spawn_action.SpawnAction;
 
-import java.util.List;
-
 public class Simulation {
     private final Map map;
-    private final MapConsoleRenderer mapConsoleRenderer;
-    private List<Action> initActions;
-    private List<Action> turnActions;
+    private static final int TIME_POINT = 150;
     private int count = 0;
-    //com.example.simulation.Map
-    //Count
-    //Рендерер поля (отрисовка)
-    //com.example.simulation.actions.Actions - список действий, исполняемых перед стартом симуляции или на каждом ходу
-
 
     public Simulation(int rows, int columns) {
         this.map = new Map(rows,columns);
-        this.mapConsoleRenderer = new MapConsoleRenderer();
-        this.initActions = List.of(new SpawnAction());
-        this.turnActions = List.of(new CreatureInteractionAction(), new RespawnHerbivoreAction());
     }
 
     public void gameLoop() {
@@ -37,25 +25,10 @@ public class Simulation {
         menu.add("Очистить карту", new ClearMapAction());
         menu.add("Выход", new ExitAction());
 
-//        menu.show();
-//        menu.select();
-//
-//        menu.show();
-//        menu.select();
-//
-//        while (count < 50) {
-////            menu.show();
-////            menu.select();
-//            mapConsoleRenderer.render(map);
-//            System.out.println();
-//            count++;
-//        }
-
-        while (true) {
+        while (count < TIME_POINT) {
             menu.show();
             menu.select();
+            count++;
         }
-
-//
     }
 }
